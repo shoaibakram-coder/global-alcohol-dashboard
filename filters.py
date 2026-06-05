be31d38f7166fba0c7817581ee2ab936f1303aa5
Shoaib <akramshoaib321123@mail.com>
2026-06-05 21:05:03 +0500

    added kpi boxes

--- filters.py
+++ filters.py
@@ -2,9 +2,45 @@ import streamlit as st
 
 def sidebar_filters(df):
 
-    st.sidebar.title(
-        "🌍 Dashboard Filters"
-    )
+    st.sidebar.markdown("""
+    <style>
+
+    section[data-testid="stSidebar"]{
+        background-color:#111827;
+    }
+
+    .sidebar-title{
+        color:#7DD3FC;
+        font-size:30px;
+        font-weight:bold;
+        text-align:center;
+        text-shadow:0px 0px 10px #38BDF8;
+        margin-bottom:10px;
+    }
+
+    .sidebar-subtitle{
+        color:#CBD5E1;
+        text-align:center;
+        font-size:14px;
+        margin-bottom:20px;
+    }
+
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.sidebar.markdown("""
+    <div class="sidebar-title">
+        🌍 Global Alcohol Dashboard
+    </div>
+
+    <div class="sidebar-subtitle">
+        Professional Data Analytics
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.sidebar.markdown("---")
+
+    st.sidebar.header("Dashboard Filters")
 
     selected_country = st.sidebar.multiselect(
         "Select Country",
